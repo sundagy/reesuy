9fab1e03668ef31e127c614ed0eb684b435e452c
Maksim Blinenkov <m.blinenkov@alarmtrade.ru>
2022-07-30 02:50:15 +0300

    widgets

--- Next/components/widget.tsx
+++ Next/components/widget.tsx
@@ -1,13 +1,48 @@
 
 import style from './widget.module.scss';
+import React from "react";
 
-const Widget = ({children}) => {
-    return (<div className={style.widget}>
-        <div className={style.header}/>
-        <div>
-            {children}
-        </div>
-    </div>)
+class Widget extends React.Component<any, any> {
+
+    private downX = 0;
+    private downY = 0;
+
+    constructor() {
+        super();
+        this.state = {
+            pressed: false,
+        }
+    }
+    render(){
+        return (<div className={style.widget}>
+            {this.state.pressed && <div
+                className={style.mouseEvents}
+                onMouseMove={(e) => {
+                    if (e.buttons !== 1) {
+                        return;
+                    }
+                    const w = (e.target as Element).parentElement;
+                    w.style.left = (e.pageX - this.downX) + 'px';
+                    w.style.top = (e.pageY - this.downY) + 'px';
+                }}
+                onMouseUp={() => {
+                    this.setState({pressed: false});
+                }}
+            />}
+            <div
+                className={style.header}
+                onMouseDown={(e) => {
+                    this.setState({pressed: true});
+                    const rect = (e.target as Element).getBoundingClientRect();
+                    this.downX = e.clientX - rect.left;
+                    this.downY = e.clientY - rect.top;
+                }}
+            />
+            <div>
+                {this.props.children}
+            </div>
+        </div>)
+    }
 }
 
 export default Widget
